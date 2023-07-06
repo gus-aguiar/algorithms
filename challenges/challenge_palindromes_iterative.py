a7f272fea96a9b363c4857933d3c8ab5fe2733bd
gus-aguiar <gustavopma90@gmail.com>
2023-07-06 19:29:23 -0300

    req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,11 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    else:
+        for i in range(len(word)):
+            if word[i] != word[-i - 1]:
+                return False
+        return True
+
+
+is_palindrome_iterative("arara")
